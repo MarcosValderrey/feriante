712cb3b0c00c3a0fee04e66f3266862b832699cd
MarcosValderrey <marcosgvalderrey@gmail.com>
2026-09-20 12:16:21 -0300

    Colorize icon

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
         <Navbar bg='body' expand='lg' className='mb-2 shadow-sm' sticky='top'>
             <Container fluid>
                 <Navbar.Brand href='/' className='d-flex align-items-center gap-2'>
-                    <img src='/images/icons/shop-32x32.png' alt='Feriante Logo' width={32} height={32} className='d-inline-block' />
+                    <img src='/images/icons/shop-primary-32x32.png' alt='Feriante Logo' width={32} height={32} className='d-inline-block' />
                     <span className='align-middle fw-bold text-secondary'>Feriante</span>
                     <span className='badge rounded-pill text-bg-primary'>{__APP_VERSION__}</span>
                 </Navbar.Brand>
